2681f971232a75b771a86078176548ebe3889470
hellenhuang66 <jiajunhuang168@126.com>
2024-09-05 13:27:00 +0000

    aaa

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template,request
+import sqlite3
+import datetime
 
 app=Flask(__name__)
 
@@ -13,6 +15,34 @@ def store_money():
 @app.route('/transfer_money',methods=["get","post"])
 def transfer_money():
     return render_template('transfer_money.html')
+
+@app.route('/admin',methods=["get","post"])
+def admin():
+    return render_template('admin.html')
+
+@app.route('/viewDB',methods=["get","post"])
+def viewDB():
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("select * from user")
+    r = ""
+    for row in c:
+        r += str(row) + "\n"
+    print(r)
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('viewDB.html')
+
+@app.route('/delDB',methods=["get","post"])
+def admin():
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("delete from user")
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('deleteDB.html')
 
 if __name__=='__main__':
     app.run()
